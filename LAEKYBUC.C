975cc471887245a0591b4ba7e34dea4d75970d73
Vignesh2002V <84083592+Vignesh2002V@users.noreply.github.com>
2023-01-02 12:50:07 +0530

    Add files via upload

---
+++ LAEKYBUC.C
@@ -0,0 +1,34 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+  int bucketsize,packets,oprate,buffer,n;
+  printf("Enter the bucket size\n");
+  scanf("%d",&bucketsize);
+
+  printf("Enter the output rate\n");
+  scanf("%d",&oprate);
+  buffer=0;
+  n=0;
+  while(n<5)
+  {
+    printf("Enter the input stream of packets\n");
+    scanf("%d",&packets);
+    buffer=buffer+packets;
+    if(buffer>=bucketsize)
+    {
+     printf("%d",buffer);
+     printf("Bucket Full\n");
+     exit(1);
+    }
+    else
+    {
+     buffer=buffer-oprate;
+     printf("%d\n",buffer);
+     n=n+1;
+    }
+    getch();
+
+  }
+
+}
